ee674651121075ee205482e380ee76bf5f494a13
Udi Talias <udi.talias@gmail.com>
2020-05-24 11:13:13 +0300

    feat(client): add watch to list

--- packages/browser/src/handlers/watch_handlers/index.ts
+++ packages/browser/src/handlers/watch_handlers/index.ts
@@ -1,3 +1,4 @@
 export { default as WindowSizeWatchHandler } from "./WindowSizeWatchHandler";
 export { default as WindowScrollWatchHandler } from "./WindowScrollWatchHandler";
 export { default as IntersectionWatchHandler } from "./IntersectionWatchHandler";
+export { default as MediaQueryWatchHandler } from "./MediaQueryWatchHandler";
